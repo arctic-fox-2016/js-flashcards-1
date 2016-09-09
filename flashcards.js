771aef2612c08dfeb0d97e82f08dce27de2043d8
Ahmad Ahmadi & Ryan Andhaka <pair+ahmadahmadi14.ryanandhaka@hacktiv8.com>
2016-09-09 14:41:50 +0700

    flashcard1

--- flashcards.js
+++ flashcards.js
@@ -1,2 +1,34 @@
 "use strict"
-// write your code here
+let prompt = require('prompt')
+let fs = require('fs')
+let data = JSON.parse(fs.readFileSync('data.json'))
+let input = 0
+
+//soal soal dari data.json
+class Soal {
+  static promptjawaban(){
+    prompt.get('jawaban',function(err, result){
+      if (result.jawaban == data[input].term){
+        console.log("Anda benar, ayo lanjut ke pertanyaan berikutnya")
+        input++
+        Soal.queue(data.length)
+      } else {
+        console.log("Anda salah, di coba lagi ya")
+        Soal.queue(data.length)
+      }
+    })
+  }
+
+
+  static queue(index){
+    if (input < index){
+      console.log("------------------------------------------------------------------")
+      console.log(data[input].definition)
+      Soal.promptjawaban()
+    } else {
+      console.log("flashcard sudah selesai")
+    }
+  }
+}
+
+Soal.queue(data.length)
