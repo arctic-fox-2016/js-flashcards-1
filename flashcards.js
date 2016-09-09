042676001a070756201b65993b1d86487198fa8c
sahbanagold <sahbanao@gmail.com>
2016-09-09 13:52:51 +0700

    flashcard v.1.0

--- flashcards.js
+++ flashcards.js
@@ -1,2 +1,44 @@
-"use strict"
+'use strict'
+const fs = require ('fs')
+const readline = require ('readline')
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
 // write your code here
+class Flashcards {
+
+  static init_questions(){
+     Flashcards.questions = JSON.parse(fs.readFileSync('data.json'))
+  }
+
+  static play(){
+      Flashcards.score = 100
+      Flashcards.quest(Flashcards.questions,0)
+  }
+
+  static quest(flash_ques,idx){
+    let answered = false
+    if(idx < flash_ques.length){
+    rl.question(flash_ques[idx].definition+" :",(user_ans)=>{
+      if(flash_ques[idx].term.toLowerCase() == user_ans.toLowerCase()) {
+        console.log("thats it!!")
+        Flashcards.quest(Flashcards.questions,idx+1)
+        }
+       else{
+        console.log("It's not that hard man!!");
+        Flashcards.score -= 5
+        Flashcards.quest(flash_ques,idx)
+      }
+    })
+    }
+    else{
+      console.log(`Your's score is ${Flashcards.score}`)
+      rl.close()
+    }
+  }
+
+}
+
+Flashcards.init_questions()
+Flashcards.play()
